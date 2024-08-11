6a999b777cb69a21b9a88546ab32e6204eca50b3
gimmeh <gimmeh@gimmeh.dev>
2024-08-11 21:34:08 +0200

    chore: Bump version

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "Item Meta"
 description = "Displays extra metadata in item tooltips."
 author = "gimmeh"
-version = "0.2.0"
+version = "0.2.1"
 forumthread = ""
 api_version = 10
 api_version_dst = 10
